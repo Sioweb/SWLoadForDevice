1ea5d9f0b31540ebf471ec6b84c7727a4baf94fb
Sascha Weidner <support@sioweb.de>
2017-09-18 21:32:37 +0200

    Update ContentDevice.php

--- elements/ContentDevice.php
+++ elements/ContentDevice.php
@@ -5,7 +5,7 @@ use Contao;
 
 /**
 * Contao Open Source CMS
-*  
+* 
 * @file ContentDevice.php
 * @class ContentDevice
 * @author Sascha Weidner
@@ -19,17 +19,21 @@ class ContentDevice extends \Frontend
 {
 	public function loadContentForDevice($objElement, $strBuffer)
 	{
-    $MobileDetect = new MobileDetect();
+		$MobileDetect = new MobileDetect();
 		$isMobile = ($MobileDetect->isTablet() || $MobileDetect->isMobile());
+		if(!empty($objElement->rsce_data)) {
+			$objElement->rsce_data = json_decode($objElement->rsce_data);
+			$objElement->sw_device = $objElement->rsce_data->sw_device;
+		}
 
 		if(
-      $objElement->sw_device == 'all' || 
-      !$objElement->sw_device || 
+			$objElement->sw_device == 'all' || 
+			!$objElement->sw_device || 
 
-      ($objElement->sw_device == 'mobile&ipad' && $isMobile) || 
-      ($objElement->sw_device == 'desktop' && !$isMobile) || 
-      ($objElement->sw_device == 'desktop&ipad' && (!$MobileDetect->isMobile() || $MobileDetect->isTablet())) || 
-      ($objElement->sw_device == 'mobile' && $isMobile && !$MobileDetect->isTablet()))
+			($objElement->sw_device == 'mobile&ipad' && $isMobile) || 
+			($objElement->sw_device == 'desktop' && !$isMobile) || 
+			($objElement->sw_device == 'desktop&ipad' && (!$MobileDetect->isMobile() || $MobileDetect->isTablet())) || 
+			($objElement->sw_device == 'mobile' && $isMobile && !$MobileDetect->isTablet()))
 			return $strBuffer;
 	}
 }
